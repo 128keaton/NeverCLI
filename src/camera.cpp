c3cf46bf4fa32ffd9126a50189c08232623cc6db
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-02 16:26:36 -0500

    Removes dep of  av_ts2timestr

--- src/camera.cpp
+++ src/camera.cpp
@@ -13,18 +13,6 @@ using json = nlohmann::json;
 using thread = std::thread;
 
 namespace never {
-    static void log_packet(const AVFormatContext *fmt_ctx, const AVPacket *pkt, const char *tag) {
-        AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
-
-        printf("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
-               tag,
-               av_ts2str(pkt->pts), av_ts2timestr(pkt->pts, time_base),
-               av_ts2str(pkt->dts), av_ts2timestr(pkt->dts, time_base),
-               av_ts2str(pkt->duration), av_ts2timestr(pkt->duration, time_base),
-               pkt->stream_index);
-    }
-
-
     Camera::Camera(const char *camera_name, const char *stream_url, const char *snapshot_url, const char *output_path) {
         this->error_count = 0;
         this->camera_name = camera_name;
