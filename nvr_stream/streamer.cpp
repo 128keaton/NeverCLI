6f63dd4e48d4128213b4f37e9ee1096de685c7a3
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-08 13:00:48 -0600

    Tinkering with tune

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -129,8 +129,10 @@ namespace nvr {
                 // h264 encode with vaapi
                 appData.encoder = gst_element_factory_make("vaapih264enc", "enc");
                 g_object_set(G_OBJECT(appData.encoder), "tune", 1, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "bitrate", 1024, nullptr);
                 g_object_set(G_OBJECT(appData.encoder), "rate-control", 4, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "quality-level", 6, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "quality-level", 7, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "quality-factor", 80, nullptr);
             }
 
 
