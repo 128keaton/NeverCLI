1edea75be9be717f663188b23e7e797e1651d17d
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-09 00:37:45 -0600

    Help me

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -144,8 +144,8 @@ namespace nvr {
                 // h264 encode with vaapi
                 appData.encoder = gst_element_factory_make("vaapih264enc", "enc");
                 g_object_set(G_OBJECT(appData.encoder), "tune", 3, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "bitrate", 1000, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "keyframe-period", 60, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "bitrate", 350, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "keyframe-period", 1, nullptr);
                // g_object_set(G_OBJECT(appData.encoder), "rate-control", 4, nullptr);
               //  g_object_set(G_OBJECT(appData.encoder), "quality-level", 6, nullptr);
                // g_object_set(G_OBJECT(appData.encoder), "quality-factor", 30, nullptr);
