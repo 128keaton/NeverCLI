47c29840b46fe5aeedb7bdd5a9a6a2c8013c1406
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-08 19:07:09 -0600

    Retune?

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -142,9 +142,9 @@ namespace nvr {
                 // h264 encode with vaapi
                 appData.encoder = gst_element_factory_make("vaapih264enc", "enc");
                 g_object_set(G_OBJECT(appData.encoder), "tune", 3, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "refs", 1, nullptr);
-             //   g_object_set(G_OBJECT(appData.encoder), "compliance-mode", 1, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "keyframe-period", 1, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "bitrate", 650, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "compliance-mode", 1, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "keyframe-period", 10, nullptr);
                 g_object_set(G_OBJECT(appData.encoder), "rate-control", 4, nullptr);
                 g_object_set(G_OBJECT(appData.encoder), "quality-level", 6, nullptr);
                 g_object_set(G_OBJECT(appData.encoder), "quality-factor", 30, nullptr);
