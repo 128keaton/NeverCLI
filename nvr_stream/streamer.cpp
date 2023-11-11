3285afe899ccf21ddafc9472c384d60c332f660e
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-10 19:12:20 -0600

    Tweaks rate control

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -139,10 +139,10 @@ namespace nvr {
                 appData.encoder = gst_element_factory_make("vaapih264enc", "enc");
 
                 g_object_set(G_OBJECT(appData.encoder), "tune", 1, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "rate-control", 1, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "max-qp", 48, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "init-qp", 40, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "min-qp", 39, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "rate-control", 7, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "max-qp", 51, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "init-qp", 45, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "min-qp", 40, nullptr);
                 g_object_set(G_OBJECT(appData.encoder), "keyframe-period", 1, nullptr);
             }
 
