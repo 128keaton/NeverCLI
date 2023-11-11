342a0a59db0ce6e7118001eaf04d714ba4c912c7
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-10 19:21:06 -0600

    Tweaks bitrate

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -138,10 +138,10 @@ namespace nvr {
                 // h264 encode with vaapi
                 appData.encoder = gst_element_factory_make("vaapih264enc", "enc");
 
-                g_object_set(G_OBJECT(appData.encoder), "tune", 1, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "rate-control", 5, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "quality-level", 6, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "quality-factor", 40, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "tune", 3, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "rate-control", 2, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "bitrate", 24000, nullptr);
+
               //  g_object_set(G_OBJECT(appData.encoder), "max-qp", 51, nullptr);
               //  g_object_set(G_OBJECT(appData.encoder), "init-qp", 45, nullptr);
               //  g_object_set(G_OBJECT(appData.encoder), "min-qp", 40, nullptr);
