4be64dde6a28deb4549d45f1d913cbcbdc754d61
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-18 17:38:27 -0600

    More qp tuning

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -154,10 +154,10 @@ namespace nvr {
 
                 //  g_object_set(G_OBJECT(appData.encoder), "qos", true, nullptr);
                 g_object_set(G_OBJECT(appData.encoder), "rate-control", 1, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "tune", 1, nullptr);
+             //   g_object_set(G_OBJECT(appData.encoder), "tune", 1, nullptr);
                 g_object_set(G_OBJECT(appData.encoder), "qp-ip", 4, nullptr);
                 g_object_set(G_OBJECT(appData.encoder), "qp-ib", 4, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "init-qp", 28, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "init-qp", 26, nullptr);
 
                 //  g_object_set(G_OBJECT(appData.encoder), "quality-level", this->quality_config.quality_level, nullptr);
                 //  g_object_set(G_OBJECT(appData.encoder), "max-bframes", this->quality_config.max_bframes, nullptr);
