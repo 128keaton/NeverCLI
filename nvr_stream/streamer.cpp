988ac285ebde9128bf8e64e4319a5ba4a22efb4d
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-18 17:40:00 -0600

    Disables some rtph264pay elements

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -103,9 +103,9 @@ namespace nvr {
 
         // h264 final payloader
         appData.payloader = gst_element_factory_make("rtph264pay", "pay");
-        g_object_set(G_OBJECT(appData.payloader), "config-interval", 1, nullptr);
-        g_object_set(G_OBJECT(appData.payloader), "pt", 96, nullptr);
-        g_object_set(G_OBJECT(appData.payloader), "aggregate-mode", 1, nullptr);
+        //g_object_set(G_OBJECT(appData.payloader), "config-interval", 1, nullptr);
+        //g_object_set(G_OBJECT(appData.payloader), "pt", 96, nullptr);
+        //g_object_set(G_OBJECT(appData.payloader), "aggregate-mode", 1, nullptr);
 
         // udp output sink
         appData.sink = gst_element_factory_make("udpsink", "udp");
