23a8112e086bdf283a35fb39aaa16ea23747a3fd
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-20 15:42:04 -0600

    MOAR threads

--- nvr_stream/janus.cpp
+++ nvr_stream/janus.cpp
@@ -352,7 +352,7 @@ namespace nvr {
         body["name"] = streamName;
         body["type"] = "rtp";
         body["id"] = streamID;
-        body["threads"] = 2;
+        body["threads"] = 4;
         body["media"] = buildMedia(streamName, streamID, port);
 
         json request = buildMessage(body);
