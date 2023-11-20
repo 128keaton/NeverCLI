89a70d548898cb04e564e8fb9178e25253324da2
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-20 17:30:48 -0600

    60 keyframe-period, 5 max bframes

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -159,8 +159,8 @@ namespace nvr {
 
                 logger->info("Using encoder parameters: {}", quality_config.toJSON().dump(4));
                 g_object_set(G_OBJECT(appData.encoder), "rate-control", 2, nullptr); // cbr (constant bitrate)
-                g_object_set(G_OBJECT(appData.encoder), "max-bframes", 15, nullptr); // max-bframes (not i-frames)
-                g_object_set(G_OBJECT(appData.encoder), "keyframe-period", 0, nullptr); // auto (duh)
+                g_object_set(G_OBJECT(appData.encoder), "max-bframes", 5, nullptr); // max-bframes (not i-frames)
+                g_object_set(G_OBJECT(appData.encoder), "keyframe-period", 60, nullptr); // auto (duh)
                 g_object_set(G_OBJECT(appData.encoder), "bitrate", 1024, nullptr); // bitrate (duh)
 
             }
