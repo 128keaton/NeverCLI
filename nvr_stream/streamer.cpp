e826731e134562c2cbe9309d3aacba4a849c6342
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-20 16:08:34 -0600

    quality-level

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -98,7 +98,7 @@ namespace nvr {
 
         // rtsp source
         appData.rtspSrc = gst_element_factory_make("rtspsrc", "src");
-        g_object_set(G_OBJECT(appData.rtspSrc), "latency", 4000, nullptr); // 5 seconds
+        g_object_set(G_OBJECT(appData.rtspSrc), "latency", 10000, nullptr); // buffer 10 seconds
         g_object_set(G_OBJECT(appData.rtspSrc), "buffer-mode", 3, nullptr); // auto
         g_object_set(G_OBJECT(appData.rtspSrc), "protocols", 0x00000004, nullptr); // tcp
         g_object_set(G_OBJECT(appData.rtspSrc), "location", rtsp_stream_location.c_str(), nullptr);
@@ -119,7 +119,8 @@ namespace nvr {
 
         // decoding/encoding queue
         appData.queue = gst_element_factory_make("rtpjitterbuffer", nullptr);
-        g_object_set(G_OBJECT(appData.queue), "latency", 4000, nullptr);
+        g_object_set(G_OBJECT(appData.queue), "latency", 10000, nullptr); // buffer 10 seconds
+        g_object_set(G_OBJECT(appData.queue), "drop-on-latency", true, nullptr); // buffer 10 seconds
 
         // rtprtxqueue
         appData.queue2 = gst_element_factory_make("rtprtxqueue", nullptr);
@@ -164,6 +165,7 @@ namespace nvr {
                 g_object_set(G_OBJECT(appData.encoder), "keyframe-period", 0, nullptr); // auto
                 g_object_set(G_OBJECT(appData.encoder), "bitrate", 1024, nullptr); // bitrate (duh)
                 g_object_set(G_OBJECT(appData.encoder), "cabac", false, nullptr); // use cabac entropy
+                g_object_set(G_OBJECT(appData.encoder), "quality-level", 5, nullptr); // use shit quality
             }
 
 
