6186983f6cbcdca18ecbc60cef94dd36c9d8d615
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-20 18:05:40 -0600

    Tries higher latency

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -120,9 +120,9 @@ namespace nvr {
 
         // decoding/encoding queue
         appData.queue = gst_element_factory_make("rtpjitterbuffer", nullptr);
-        g_object_set(G_OBJECT(appData.queue), "latency", 400, nullptr);
+        g_object_set(G_OBJECT(appData.queue), "latency", 25000, nullptr);
         g_object_set(G_OBJECT(appData.queue), "faststart-min-packets", 25, nullptr);
-        g_object_set(G_OBJECT(appData.queue), "max-misorder-time", 4000, nullptr); // buffer 1 second
+        g_object_set(G_OBJECT(appData.queue), "max-misorder-time", 1500, nullptr); // 1.5 seconds
 
         // rtprtxqueue
         appData.queue2 = gst_element_factory_make("rtprtxqueue", nullptr);
