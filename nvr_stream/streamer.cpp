0022b1a522e302ddb8349e98839bb579db48a0c7
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-21 19:04:55 -0600

    Back to queue2

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -146,8 +146,8 @@ namespace nvr {
         g_object_set(G_OBJECT(appData.initialQueue), "max-size-time", max_delay * 2, nullptr);
         g_object_set(G_OBJECT(appData.initialQueue), "max-size-buffers", max_buffers, nullptr);
 
-        appData.finalQueue = gst_element_factory_make("queue", "final_queue");
-        g_object_set(G_OBJECT(appData.finalBufferQueue), "min-threshold-time",  delay, nullptr);
+        appData.finalQueue = gst_element_factory_make("queue2", "final_queue");
+    //    g_object_set(G_OBJECT(appData.finalBufferQueue), "min-threshold-time",  delay, nullptr);
         g_object_set(G_OBJECT(appData.finalQueue), "max-size-bytes", max_bytes_size * 2, nullptr);
         g_object_set(G_OBJECT(appData.finalQueue), "max-size-time", max_delay * 2, nullptr);
         g_object_set(G_OBJECT(appData.finalQueue), "max-size-buffers", max_buffers, nullptr);
