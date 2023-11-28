47326f74eb74ba8f6919f1dd1eb9999df268cf01
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-28 12:29:00 -0600

    Fixes h264 pipeline

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -266,14 +266,12 @@ namespace nvr {
             // add everything
             gst_bin_add_many(GST_BIN(appData.pipeline), appData.rtspSrc, appData.initialQueue, appData.dePayloader,
                              appData.parser,
-                             appData.finalBufferQueue,
                              appData.payloader,
                              appData.finalQueue,
                              appData.sink, nullptr);
 
             // link everything except source
-            gst_element_link_many(appData.initialQueue, appData.dePayloader, appData.parser, appData.finalBufferQueue,
-                                  appData.payloader, appData.finalQueue, appData.sink, NULL);
+            gst_element_link_many(appData.initialQueue, appData.dePayloader, appData.parser, appData.payloader, appData.finalQueue, appData.sink, NULL);
         }
 
         g_signal_connect(appData.rtspSrc, "pad-added", G_CALLBACK(nvr::Streamer::padAddedHandler), &appData);
