bd8afef2955d22109fba790ad839f85546bc7571
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-28 11:45:55 -0600

    Still tinkering

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -210,7 +210,7 @@ namespace nvr {
                 //    g_object_set(G_OBJECT(appData.encoder), "gop-size", 20, nullptr);
                 //  g_object_set(G_OBJECT(appData.encoder), "bitrate", 5120, nullptr);
                 //      g_object_set(G_OBJECT(appData.encoder), "min-force-key-unit-interval", min_delay, nullptr);
-                g_object_set(G_OBJECT(appData.encoder), "rc-mode", 3, nullptr); // cbr
+              //  g_object_set(G_OBJECT(appData.encoder), "rc-mode", 3, nullptr); // cbr
                 //         g_object_set(G_OBJECT(appData.encoder), "vbv-buffer-size", max_buffers, nullptr);
             }
             else if (this->has_vaapi && !this->has_nvidia) {
