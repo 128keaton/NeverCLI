f91aa65cef836c0bf54b63beb51674b6c39163f7
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-28 10:29:27 -0600

    Still working out why we see green

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -214,11 +214,11 @@ namespace nvr {
 
           //      g_object_set(G_OBJECT(appData.encoder), "preset", 5, nullptr); // low-latency-hp
                 g_object_set(G_OBJECT(appData.encoder), "gop-size", 20, nullptr);
-               g_object_set(G_OBJECT(appData.encoder), "bitrate", 1500, nullptr);
-            //   g_object_set(G_OBJECT(appData.encoder), "min-force-key-unit-interval", 250, nullptr);
+               g_object_set(G_OBJECT(appData.encoder), "bitrate", 950, nullptr);
+               g_object_set(G_OBJECT(appData.encoder), "min-force-key-unit-interval", min_delay, nullptr);
                 g_object_set(G_OBJECT(appData.encoder), "rc-mode", 5, nullptr); // cbr
                 //         g_object_set(G_OBJECT(appData.encoder), "vbv-buffer-size", max_buffers, nullptr);
-          //      g_object_set(G_OBJECT(appData.encoder), "qos", true, nullptr);
+                g_object_set(G_OBJECT(appData.encoder), "zerolatency", true, nullptr);
 
             }
             else if (this->has_vaapi && !this->has_nvidia) {
