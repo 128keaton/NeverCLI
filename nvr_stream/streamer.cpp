aaa837dbc125cac1e1a1593e4d2b20a6de1c30b0
Keaton Burleson <keaton@vipindoorstorage.com>
2023-11-28 14:23:54 -0600

    Trying SRT

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -243,7 +243,6 @@ namespace nvr {
             gst_bin_add_many(
                 GST_BIN(appData.pipeline),
                 appData.rtspSrc,
-                appData.finalBufferQueue,
                 appData.initialQueue,
                 appData.dePayloader,
                 appData.parser,
@@ -258,7 +257,6 @@ namespace nvr {
             // link everything except source
             gst_element_link_many(
             appData.rtspSrc,
-            appData.finalBufferQueue,
                 appData.initialQueue,
                 appData.dePayloader,
                 appData.parser,
@@ -414,7 +412,7 @@ namespace nvr {
     }
 
     void Streamer::padAddedHandler(GstElement* src, GstPad* new_pad, StreamData* data) {
-        GstPad* sink_pad = gst_element_get_static_pad(data->finalBufferQueue, "sink");
+        GstPad* sink_pad = gst_element_get_static_pad(data->initialQueue, "sink");
         GstPadLinkReturn ret;
         GstCaps* new_pad_caps = nullptr;
         GstStructure* new_pad_struct;
