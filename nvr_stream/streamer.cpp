745fcc257442a71f876fb3b3debc428d15dd2b60
Keaton Burleson <keaton@vipindoorstorage.com>
2023-12-14 15:34:15 -0600

    Removes trellis

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -211,7 +211,6 @@ namespace nvr {
             appData.encoder = gst_element_factory_make("vaapivp8enc", "enc");
             g_object_set(G_OBJECT(appData.encoder), "rate-control", 2, nullptr);
             g_object_set(G_OBJECT(appData.encoder), "bitrate", 400, nullptr);
-            g_object_set(G_OBJECT(appData.encoder), "trellis", true, nullptr);
             g_object_set(G_OBJECT(appData.encoder), "max-bframes", 1, nullptr);
         }
 
