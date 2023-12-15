01851afcca7bff06c0c9506eeb82936924edaf31
Keaton Burleson <keaton@vipindoorstorage.com>
2023-12-15 00:20:52 -0600

    Testing disabling queues/buffers

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -122,7 +122,7 @@ namespace nvr {
         g_object_set(G_OBJECT(appData.rtspSrc), "location", rtsp_stream_location.c_str(), nullptr);
         g_object_set(G_OBJECT(appData.rtspSrc), "message-forward", true, nullptr);
         g_object_set(G_OBJECT(appData.rtspSrc), "ntp-sync", true, nullptr);
-        g_object_set(G_OBJECT(appData.rtspSrc), "udp-buffer-size", 1073741823, nullptr);
+      //  g_object_set(G_OBJECT(appData.rtspSrc), "udp-buffer-size", 1073741823, nullptr);
         g_object_set(G_OBJECT(appData.rtspSrc), "udp-reconnect", true, nullptr);
         g_object_set(G_OBJECT(appData.rtspSrc), "user-id", this->rtsp_username.c_str(), nullptr);
         g_object_set(G_OBJECT(appData.rtspSrc), "user-pw", this->rtsp_password.c_str(), nullptr);
@@ -219,26 +219,26 @@ namespace nvr {
         gst_bin_add_many(
                 GST_BIN(appData.pipeline),
                 appData.rtspSrc,
-                appData.initialQueue,
+           //     appData.initialQueue,
                 appData.dePayloader,
                 appData.parser,
                 appData.decoder,
                 appData.encoder,
                 appData.payloader,
-                appData.finalQueue,
+        //        appData.finalQueue,
                 appData.sink,
                 nullptr
         );
 
         // link everything except source
         gst_element_link_many(
-                appData.initialQueue,
+       //         appData.initialQueue,
                 appData.dePayloader,
                 appData.parser,
                 appData.decoder,
                 appData.encoder,
                 appData.payloader,
-                appData.finalQueue,
+       //         appData.finalQueue,
                 appData.sink,
                 NULL);
 
