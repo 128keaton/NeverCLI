4c75cbc1ca4d2f32f6559cd1fddd7e3c55f51c1f
Keaton Burleson <keaton@vipindoorstorage.com>
2024-01-23 12:04:56 -0600

    Encoder tweaking

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -519,6 +519,11 @@ namespace nvr {
                 appData->encoder = gst_element_factory_make("vaapivp8enc", "enc");
                 g_object_set(G_OBJECT(appData->encoder), "rate-control", 2, nullptr);
                 g_object_set(G_OBJECT(appData->encoder), "bitrate", appData->bitrate, nullptr);
+                g_object_set(G_OBJECT(appData->encoder), "trellis", true, nullptr);
+                g_object_set(G_OBJECT(appData->encoder), "keyframe-period", 0, nullptr);
+                g_object_set(G_OBJECT(appData->encoder), "min-force-key-unit-interval", 1000000000, nullptr);
+                g_object_set(G_OBJECT(appData->encoder), "quality-level", 3, nullptr);
+                g_object_set(G_OBJECT(appData->encoder), "yac-qi", 14, nullptr);
             }
         }
     }
