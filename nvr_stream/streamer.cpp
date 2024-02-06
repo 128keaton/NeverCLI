446841af2b6c9a98f48d63f7617cc5447c6ce92c
Keaton Burleson <keaton@vipindoorstorage.com>
2024-02-06 10:57:40 -0600

    tweaks for u3/h264

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -97,6 +97,7 @@ namespace nvr {
             g_object_set(G_OBJECT(appData.payloader), "config-interval", -1, nullptr);
             g_object_set(G_OBJECT(appData.payloader), "mtu", 1200, nullptr);
             g_object_set(G_OBJECT(appData.payloader), "onvif-no-rate-control", true, nullptr);
+            g_object_set(G_OBJECT(appData.payloader), "perfect-rtptime", false, nullptr);
         } else {
             // vp8 final payloader
             appData.payloader = gst_element_factory_make("rtpvp8pay", "pay");
