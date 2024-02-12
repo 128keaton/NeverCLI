065bf38fbce76654cdc8293ce6f666a412c7c9bd
Keaton Burleson <keaton@vipindoorstorage.com>
2024-02-12 14:14:25 -0600

    trying to fix rtp port issue

--- nvr_stream/streamer.cpp
+++ nvr_stream/streamer.cpp
@@ -358,7 +358,8 @@ namespace nvr {
             data->logger->warn("Stream created, but unable to notify Janus, trying to recreate stream");
             data->error_count += 1;
             data->logger->info("Retrying stream creation");
-            sleep(1);
+            sleep(10);
+            data->rtp_port += 1;
             return createJanusStream(data);
         }
     }
